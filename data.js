cccf1bddb28d4957a883e9b214b42fd51adf891f
Nam485 <namitgoyal485@gmail.com>
2024-06-21 12:35:56 +0530

    infinite logo slider

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+    img1 : "./running-1.svg",
+    img2 : "./running-2.svg",
+    img3 : "./running-3.svg",
+    img4 : "./running-4.svg",
+    img5 : "./running-5.svg",
+    img6 : "./running-6.svg",
+    img7 : "./running-7.svg",
+    img8 : "./running-8.svg",
+    img9 : "./running-9.svg",
+    img10 : "./running-10.svg",
+    
+}
